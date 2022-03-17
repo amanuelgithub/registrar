2c0063d143c2caeb9fc90f88cfc86aed12a1880e
amanuelgithub <amanuelgirma070@gmail.com>
2022-03-17 12:35:16 +0600

    sidenav toggling completed

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'registrar';
   panelOpenState = false;
+
+  @ViewChild('sidenav', null) sidenav: MatSidenav;
+
+  sidenavOpened = true;
+
+  close() {
+    this.sidenav.close();
+    this.sidenavOpened = false;
+  }
+
+  open(){
+    this.sidenav.open();
+    this.sidenavOpened = true;
+  }
 }
